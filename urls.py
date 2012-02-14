4c8d38445ca40322a6c5e116b6b52820605721e6
Dylan Nugent <dylnuge@gmail.com>
2012-02-14 17:49:18 -0600

    Hey, a working site

--- urls.py
+++ urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'treasure_chest.views.home', name='home'),
     # url(r'^treasure_chest/', include('treasure_chest.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Administration controls
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
