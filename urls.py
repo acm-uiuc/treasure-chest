1cb82f4ca55538d7f499fd922ffc3fa3c067b20a
Dylan Nugent <dylnuge@gmail.com>
2012-04-03 18:27:02 -0500

    Adding basic bootstrap assets for site layout.
    As part of #5, adding new assets to facilitate making a nice layout.
    Also adding a static link to the assets. On the development server, we
    can make this cleaner by using a site root and a local_settings.py file.
    On the production server, this should be handled by whatever production
    server is running (i.e. Apache).

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 
 admin.autodiscover()
@@ -15,3 +16,10 @@ urlpatterns += patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG == True:
+    urlpatterns += patterns('',
+        # Static files (development server only)
+        (r'^assets/(?P<path>.*)', 'django.views.static.serve',
+            { 'document_root' : settings.ASSETS_ROOT })
+    )
