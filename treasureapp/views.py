62ceefb0e78482a649038c448e52114029c47884
Dylan Nugent <dylnuge@gmail.com>
2012-04-05 00:25:24 -0500

    Reorganizing and adding stubs.
    Moved main.html to base.html and added bootstrap forms app to help sync
    with acm-uiuc/liquid when the time comes. Also added stubs for all the
    views needed for milestone 1.

--- treasureapp/views.py
+++ treasureapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
+# Basic content handlers
+
 def index(request):
     """
     Render the main page of the Treasure Chest application.
@@ -9,10 +11,49 @@ def index(request):
     context = RequestContext(request, {})
     return render_to_response("index.html", context)
 
-def accounts(request):
+def help(request):
+    pass
+
+# Account handlers
+
+def account_list(request):
+    """
+    Render the listing of all accounts.
+
+    On GET, it will return a listing of all accounts.
+    On POST, it will create a new account.
+    """
+
     context = RequestContext(request, {})
     return render_to_response("index.html", context)
 
 def account_detail(request):
+    """
+    Handle the REST features on individual accounts.
+
+    On GET, it will return details on the specific account.
+    On POST, it will update information about the account.
+    """
+
     context = RequestContext(request, {})
     return render_to_response("index.html", context)
+
+def account_create(request):
+    pass
+
+def account_update(request):
+    pass
+
+# Transaction handlers
+
+def transaction_list(request):
+    pass
+
+def transaction_detail(request):
+    pass
+
+def transaction_create(request):
+    pass
+
+def transaction_update(request):
+    pass
--- urls.py
+++ urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('treasureapp.views',
     # Treasureapp URLs
-    url(r'^/account/(?P<account_id>\d+)$', 'account_detail', {}, 'account_detail'),
-    url(r'^/account/$', 'accounts', {}, 'account_list'),
+    url(r'^account/(?P<account_id>\d+)$', 'account_detail', {}, 'account_detail'),
+    url(r'^account/$', 'account_list', {}, 'account_list'),
     url(r'^$', 'index', {}, 'index'),
 )
 
