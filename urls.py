48d64af03cd8b38c40ba7a34538c7c85b81278f2
Dylan Nugent <dylnuge@gmail.com>
2012-04-12 16:28:46 -0500

    Finished adding user authentication. Still needs to look pretty.
    This should resolve issue #10, but I want to polish this a lot more
    because it is very ugly right now on the frontend. I also want to have
    user account details, etc.

--- urls.py
+++ urls.py
@@ -27,6 +27,7 @@ urlpatterns = patterns('treasureapp.views',
 urlpatterns += patterns('',
     # Registration controls
     (r'^', include('registration.backends.default.urls')),
+    (r'^', include('registration.auth_urls')),
     # Administration controls
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
