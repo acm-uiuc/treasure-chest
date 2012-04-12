81b987311a6aab2fdc2a6552c1d54bc24fedd1e5
Dylan Nugent <dylnuge@gmail.com>
2012-04-12 13:09:02 -0500

    Adding a bunch of templates and controls for account creation.
    Working towards having user authentication for issue #10.

--- urls.py
+++ urls.py
@@ -25,6 +25,8 @@ urlpatterns = patterns('treasureapp.views',
 )
 
 urlpatterns += patterns('',
+    # Registration controls
+    (r'^', include('registration.backends.default.urls')),
     # Administration controls
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
