a2bfcc023aeb632d694fe950244228c1daa7cae7
Dylan Nugent <dylnuge@gmail.com>
2012-04-29 23:39:39 -0500

    One line to resolve #16, one line to resolve them all

--- treasureapp/forms.py
+++ treasureapp/forms.py
@@ -3,31 +3,31 @@ from django.forms import Form, ModelForm, DecimalField
 from treasureapp.models import Account, Transaction, AccountGroup
 
 class AccountForm(ModelForm):
-    """
-    A basic form for creating or updating an account.
-    """
+	"""
+	A basic form for creating or updating an account.
+	"""
 
-    class Meta:
-        model = Account
-        # User cannot directly edit cached balance
-        fields = ('name', 'description')
+	class Meta:
+		model = Account
+		# User cannot directly edit cached balance
+		fields = ('name', 'description', 'accessors')
 
 class TransactionForm(ModelForm):
-    """
-    A basic form for creating or updating a transaction.
-    """
+	"""
+	A basic form for creating or updating a transaction.
+	"""
 
-    # Force the decimal number to be positive
-    amount = DecimalField(min_value=0, max_digits=10, decimal_places=2)
+	# Force the decimal number to be positive
+	amount = DecimalField(min_value=0, max_digits=10, decimal_places=2)
 
-    class Meta:
-        model = Transaction
-        fields = ('to_acct', 'amount', 'description')
+	class Meta:
+		model = Transaction
+		fields = ('to_acct', 'amount', 'description')
 
 class AccountGroupForm(ModelForm):
-    """
-    A form for creating and updating user groups.
-    """
+	"""
+	A form for creating and updating user groups.
+	"""
 
-    class Meta:
-        model = AccountGroup
+	class Meta:
+		model = AccountGroup
