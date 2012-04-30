b576627c9f52a86096747b48744b067fe4269a39
Dylan Nugent <dylnuge@gmail.com>
2012-04-29 22:40:00 -0500

    Stubbing out a Member class for future usage.
    Taking a page from acm-uiuc/liquid and including an extendable user
    class. @reedlabotz knows what he's doing.

--- treasureapp/models.py
+++ treasureapp/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
+class Member(User):
+	pass
+
 class AccountGroup(models.Model):
 	name = models.CharField(max_length=30)
-	members = models.ManyToManyField(User, through="GroupMember")
+	members = models.ManyToManyField(Member, through="GroupMember")
 
 	def __unicode__(self):
 		return self.name
 
 class GroupMember(models.Model):
 	group = models.ForeignKey(AccountGroup)
-	member = models.ForeignKey(User)
+	member = models.ForeignKey(Member)
 
 	class Meta:
 		unique_together = (("group", "member"))
